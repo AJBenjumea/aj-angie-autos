b8060e2c9034904689f21c1b721ce207b4daa583
AngieD8615 <adavidson1@gmail.com>
2021-05-10 15:37:18 -0600

    created Auto class

--- src/main/java/com/automobile/autodealer/Auto.java
+++ src/main/java/com/automobile/autodealer/Auto.java
@@ -1,4 +1,61 @@
 package com.automobile.autodealer;
 
 public class Auto {
+    private int year;
+    private String make;
+    private String model;
+    private String color;
+    private String owner;
+    private String vin;
+
+    public Auto() {
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
 }
