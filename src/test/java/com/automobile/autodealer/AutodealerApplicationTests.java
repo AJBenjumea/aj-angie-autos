6fa994f554a293b64cecd9c87eba97e8b175906e
AJ Benjumea <aj@AJs-Mac-mini.fios-router.home>
2021-05-13 16:56:05 -0400

    Add applications.properties JPA Props and Env variables for MYSQL

--- src/test/java/com/automobile/autodealer/AutodealerApplicationTests.java
+++ src/test/java/com/automobile/autodealer/AutodealerApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application-test.properties")
 class AutodealerApplicationTests {
 
 	@Autowired
